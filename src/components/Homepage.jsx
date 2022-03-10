b23093dd1128dcba0499621a2caeb031a4429a1a
msek986 <99246023+msek986@users.noreply.github.com>
2022-03-10 17:45:43 +0100

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -53,7 +53,6 @@ const Homepage = () => {
         </Title>
       </div>
       <Cryptocurrencies simplified />
-      {/* <Cryptocurrencies simplified /> */}
       <div className="home-heading-container">
         <Title level={2} className="home-title">
           Latest Crypto News
